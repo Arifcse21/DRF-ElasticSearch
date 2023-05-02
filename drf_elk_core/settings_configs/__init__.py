5f65f46d201e24f087a27a6efda57da1c2cb3b30
Arifcse21 <arifcse21@gmail.com>
2023-05-02 16:45:30 +0600

    Project structure changed

---
+++ drf_elk_core/settings_configs/__init__.py
@@ -0,0 +1,7 @@
+from .applications import INSTALLED_APPS
+from .auth_pass_validators import AUTH_PASSWORD_VALIDATORS
+from .db import DATABASES
+from .elasticsearch import *
+from .i18n import *
+from .middlewares import MIDDLEWARE
+from .templates import TEMPLATES
